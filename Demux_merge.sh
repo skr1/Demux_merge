74d07f3e89d52918de0c0360042e4db68ed25136
skr1 <ssukhadia711@gmail.com>
2017-02-21 14:14:16 -0500

    Create demux_merge.sh

---
+++ Demux_merge.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+##A Script to merge sample-specific fastqs spreaded across different lanes.
+##Author: Shrey Sukhadia.
+##Run it as: ./Merge.sh
+##To execute this script you should be already inside the "Unaligned" folder under the Hiseq run-folder. Put this script under "Unaligned" folder and then execute it from there.
+
+for j in `awk -F "," '{print $3}' ../../SampleSheet.csv | grep -v 'Sample_ID' | sort |uniq`                                                                                                     [ 2:21PM]
+do
+zcat Sample_$j/$j*R1*gz > Sample_$j/$j.R1.fastq
+echo "R1 done for Sample_$j"
+zcat Sample_$j/$j*R2*gz > Sample_$j/$j.R2.fastq
+echo "R2 done for Sample_$j"
+zcat Sample_$j/$j*R3*gz > Sample_$j/$j.R3.fastq
+echo "R3 done for Sample_$j"
+gzip Sample_$j/*fastq
+done
